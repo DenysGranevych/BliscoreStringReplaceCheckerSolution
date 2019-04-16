6040914b891aa607f4ccf79a65fa2e71f2cd9568
denys.granevych <denys.granevych@gmail.com>
2019-04-16 15:18:05 +0300

    Add project files.

---
+++ StringReplaceCheckerSolution/StringReplaceChecker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace StringReplaceCheckerSolution
+{
+    public class StringReplaceChecker
+    {
+        public string Solution(string S, string T)
+        {
+            if (S == T)
+            {
+                return "EQUAL";
+            }
+
+            if (S.Length + 1 == T.Length)
+            {
+                for (int i = 0; i < S.Length; i++)
+                {
+                    if (S[i] == T[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var insertLetter = T[i];
+                        if (String.Compare(S.Substring(i), T.Substring(i + 1)) == 0)
+                        {
+                            return $"INSERT {insertLetter}";
+                        }
+                    }
+                }
+            }
+
+            if (S.Length == T.Length)
+            {
+                for (int i = 0; i < S.Length; i++)
+                {
+                    if (S[i] == T[i])
+                    {
+                        continue;
+                    }
+
+                    var replacedLetters = (s: S[i], t: T[i]);
+                    if (S.Replace(replacedLetters.s, replacedLetters.t) == T)
+                    {
+                        return $"REPLACE {replacedLetters.s} {replacedLetters.t}";
+                    }
+
+                    var swapLetters = (s1: S[i], s2: S[i + 1]);
+                    var swapedString = S.Substring(0, i) + swapLetters.s2 + swapLetters.s1 + new string(S.Skip(i + 2).ToArray());
+
+                    if (swapedString == T)
+                    {
+                        return $"SWAP {swapLetters.s1} {swapLetters.s2}";
+                    }
+                }
+            }
+
+            return "IMPOSSIBLE";
+        }
+    }
+}
---
+++ StringReplaceCheckerSolutionTests/StringReplaceCheckerTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringReplaceCheckerSolution.Tests
+{
+    [TestClass()]
+    public class StringReplaceCheckerTests
+    {
+        StringReplaceChecker solution;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            solution = new StringReplaceChecker();
+        }
+
+        [TestMethod()]
+        public void EqualTest()
+        {
+            var actual = solution.Solution("equal", "equal");
+            var expected = "EQUAL";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            var actual = solution.Solution("nice", "niece");
+            var expected = "INSERT e";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [DataRow("form", "from", "SWAP o r")]
+        [DataRow("for", "fro", "SWAP o r")]
+        public void SWAPTest(string s, string t, string expected)
+        {
+            var actual = solution.Solution(s, t);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ReplaceTest()
+        {
+            var actual = solution.Solution("test", "tent");
+            var expected = "REPLACE s n";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ImpossibleTest()
+        {
+            var actual = solution.Solution("o", "odd");
+            var expected = "IMPOSSIBLE";
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
